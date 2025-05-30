fa93a74454461b5c1caba3a699dd1c98392aa69e
Pe-te-r <mburupeter8526@gmail.com>
2025-05-30 10:15:34 +0300

    routes start

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProfilesModule } from './profiles/profiles.module';
+import { AuthorsModule } from './authors/authors.module';
+import { BooksModule } from './books/books.module';
+import { CategoryModule } from './category/category.module';
+import { BookReviewModule } from './book-review/book-review.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UsersModule,
+    ProfilesModule,
+    AuthorsModule,
+    BooksModule,
+    CategoryModule,
+    BookReviewModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
